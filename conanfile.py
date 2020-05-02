0681b1436018725cd11a180ae78c11f3255bf8d5
Alexander Zaitsev <zamazan4ik@tut.by>
2020-05-02 18:14:36 +0300

    Add initial Conan recipe for HSM library

--- conanfile.py
+++ conanfile.py
@@ -2,16 +2,41 @@ from conans import ConanFile, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    description = "Keep it short"
-    topics = ("conan", "libname", "logging")
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+class HsmConan(ConanFile):
+    name = "hsm"
+    description = "Finite state machine library based on the boost hana meta programming library"
+    topics = ("conan", "hsm", "finite-state-machine")
+    url = "https://github.com/bincrafters/conan-hsm"
+    homepage = "https://github.com/erikzenker/hsm"
+    license = "MIT"
+    settings = "compiler"
     no_copy_source = True
+    requires = "boost/1.72.0"
 
-    _source_subfolder = "source_subfolder"
+    @property
+    def _source_subfolder(self):
+        return os.path.join(self.source_folder, "source_subfolder")
+
+    def configure(self):
+        minimal_cpp_standard = "17"
+        if self.settings.compiler.cppstd:
+            tools.check_min_cppstd(self, minimal_cpp_standard)
+        minimal_version = {
+            "gcc": "6",
+            "clang": "3.9",
+            "apple-clang": "10",
+            "Visual Studio": "17"
+        }
+        compiler = str(self.settings.compiler)
+        if compiler not in minimal_version:
+            self.output.warn(
+                "%s recipe lacks information about the %s compiler standard version support." % (self.name, compiler))
+            self.output.warn(
+                "%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
+            return
+        version = tools.Version(self.settings.compiler.version)
+        if version < minimal_version[compiler]:
+            raise ConanInvalidConfiguration("%s requires a compiler that supports at least C++%s." % (self.name, minimal_cpp_standard))
 
     def source(self):
         tools.get(**self.conan_data["sources"][self.version])
@@ -21,7 +46,7 @@ class LibnameConan(ConanFile):
     def package(self):
         include_folder = os.path.join(self._source_subfolder, "include")
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
+        self.copy(pattern="*", dst=os.path.join("src", "include"), src=include_folder)
 
     def package_id(self):
         self.info.header_only()
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,63 @@
-// #include <cpprest/json.h>
+#include <hsm/hsm.h>
+
+#include <cassert>
+
+// States
+struct Locked {
+};
+struct Unlocked {
+};
+
+// Events
+struct Push {
+};
+struct Coin {
+};
+
+// Guards
+const auto noError = [](auto /*event*/, auto /*source*/, auto /*target*/){return true;};
+
+// Actions
+const auto beep = [](auto /*event*/, auto /*source*/, auto /*target*/){ std::cout << "beep!" << std::endl;};
+const auto blink = [](auto /*event*/, auto /*source*/, auto /*target*/){ std::cout << "blink, blink, blink!" << std::endl;};
+
+struct Turnstile {
+    static constexpr auto make_transition_table()
+    {
+        // clang-format off
+        return hsm::transition_table(
+            // Source               + Event               [Guard]   / Action = Target
+            // +--------------------+---------------------+---------+--------+------------------------+
+            hsm::state<Locked> {}   + hsm::event<Push> {}           / beep   = hsm::state<Locked> {}  ,
+            hsm::state<Locked> {}   + hsm::event<Coin> {} [noError] / blink  = hsm::state<Unlocked> {},
+            // +--------------------+---------------------+---------+--------+------------------------+
+            hsm::state<Unlocked> {} + hsm::event<Push> {} [noError]          = hsm::state<Locked> {}  ,
+            hsm::state<Unlocked> {} + hsm::event<Coin> {}           / blink  = hsm::state<Unlocked> {}
+            // +--------------------+---------------------+---------+--------+------------------------+
+            );
+        // clang-format on
+    }
+
+    static constexpr auto initial_state()
+    {
+        return hsm::initial(hsm::state<Locked> {});
+    }
+};
 
 int main()
 {
-    // const auto parsed_value = web::json::value::parse(U("-22"));
+    hsm::sm<Turnstile> turnstileSm;
+
+    // The turnstile is initially locked
+    assert(turnstileSm.is(hsm::state<Locked> {}));
+
+    // Inserting a coin unlocks it
+    turnstileSm.process_event(Coin {});
+    assert(turnstileSm.is(hsm::state<Unlocked> {}));
+
+    // Entering the turnstile will lock it again
+    turnstileSm.process_event(Push {});
+    assert(turnstileSm.is(hsm::state<Locked> {}));
+
+    return 0;
 }
